ddfef46b1076587323a14e768cc0d076bf684fdd
miyakz <miyakz1192@gmail.com>
2023-09-20 01:15:24 +0900

    UnitTestサーバの修正。PUT時にクライアントからのすべてのデータを読み込むように修正

--- python_http_server.py
+++ python_http_server.py
@@ -12,16 +12,23 @@ class MyHandler(CGIHTTPRequestHandler):
             # PUTデータのサイズを取得
             content_length = int(self.headers['Content-Length'])
 
-            print("受信データを読み込みます")
+            print("受信データを読み込みます. content_length=", content_length)
         
-            # PUTデータを取得
-            data = self.rfile.read(content_length)
-            print("受信データ = ", data)
         
             # ここでパスとデータを処理できます
             # 例: パスに基づいてファイルにデータを書き込む
             with open(path, 'wb') as f:
-                f.write(data)
+                # データを少しずつ読み取り、ファイルに書き込む
+                while content_length > 0:
+                    chunk = self.rfile.read(min(1024, content_length))  # 最大1024バイトずつ読み取る
+                    if not chunk:
+                        print("not chunk")
+                        break
+                    print("受信データtype = ", type(chunk))
+                    print("受信データlen = ", len(chunk))
+                    f.write(chunk)
+                    content_length -= len(chunk)
+
         
             # 応答を返す
             self.send_response(200)
